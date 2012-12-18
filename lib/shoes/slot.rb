kevinbruyne0125/shoes4

commit 26f4a2588604e201dd5f6a12d7f1d17812dcd3f7

Implement scrollbar. Try out this snippet:
Shoes.app width: 300, height: 300 do
  para 'hello shoes ' * 50
end

lib/shoes/swt/app.rb
@@ -33,6 +33,10 @@ module Shoes
         @shell.addListener(::Swt::SWT::Close, main_window_on_close) if main_app?
         @real.addMouseMoveListener MouseMoveListener.new(self)
         @real.addMouseListener MouseListener.new(self)
+        
+        vb = @shell.getVerticalBar
+        vb.setIncrement 10
+        vb.addSelectionListener SelectionListener.new(self, vb)
       end
 
       attr_reader :dsl, :real, :shell, :dx, :dy
@@ -76,7 +80,7 @@ module Shoes
       end
 
       def main_window_style
-        style  = ::Swt::SWT::CLOSE | ::Swt::SWT::MIN | ::Swt::SWT::MAX
+        style  = ::Swt::SWT::CLOSE | ::Swt::SWT::MIN | ::Swt::SWT::MAX | ::Swt::SWT::V_SCROLL
         style |= ::Swt::SWT::RESIZE if @dsl.opts[:resizable]
         style
       end
@@ -129,6 +133,19 @@ module Shoes
       end
       def mouseDoubleClick(e)
         # do nothing
+      end
+    end
+    
+    class SelectionListener
+      def initialize app, vb
+        @app, @vb = app, vb
+      end
+      def widgetSelected e
+        unless e.detail == ::Swt::SWT::DRAG
+          location = @app.real.getLocation
+          location.y = -@vb.getSelection
+          @app.real.setLocation location
+        end
       end
     end
   end

lib/shoes/swt/layout.rb
@@ -3,7 +3,22 @@ module Shoes::Swt
     attr_accessor :top_slot
 
     def layout *args
-      contents_alignment @top_slot
+      app = @top_slot.app
+      w, h = app.width, app.height
+      scrollable_height = contents_alignment @top_slot
+      app = app.gui
+      app.real.setSize w, [scrollable_height, h].max
+      vb = app.shell.getVerticalBar
+      vb.setVisible(scrollable_height > h)
+      if scrollable_height > h
+        vb.setThumb h * h / scrollable_height
+        vb.setMaximum scrollable_height - h + vb.getThumb
+        vb.setIncrement h / 2
+      else
+        location = app.real.getLocation
+        location.y = 0
+        app.real.setLocation location
+      end
     end
 
     def contents_alignment slot

commit bba111a8ab9bb10637ba6e3e8a0ec819080e815f

Fix app height calculation (was breaking specs on OS X)
Two specs were broken by commit
26f4a2588604e201dd5f6a12d7f1d17812dcd3f7, apparently only on OS X.

The spec for App#height using defaults was breaking on OS X because the
app height was including the window decorations (22px).

The spec for setting App#width was breaking on OS X because the spec set
the width to 1, but the window can't be resized smaller than 69px. This
changeset uses a higher width for the spec.

In addition, this fix lets us remove some hacks around background
rendering.

lib/shoes/slot.rb
@@ -18,11 +18,13 @@ module Shoes
         instance_variable_set "@#{v}", opts[v.to_sym]
       end
 
+      @left ||= 0
+      @top ||= 0
       @width ||= 1.0
       @height ||= 0
       @init_height = @height
       @blk = blk
-      
+
       set_margin
 
       @gui = Shoes.configuration.backend_for(self, @parent.gui)

lib/shoes/swt/app.rb
@@ -88,20 +88,15 @@ module Shoes
     class ShellControlListener
       def initialize(app)
         @app = app
-        @times_run = 0
       end
 
       def controlResized(e)
         shell = e.widget
-        w, h = shell.getSize.x - @app.dx, shell.getSize.y - @app.dy
-
-        # will break background element if it's run the first two times
-        if @times_run > 1
-          (@app.dsl.top_slot.width, @app.dsl.top_slot.height = w, h)
-        else
-          @times_run += 1
-        end
+        client_area = shell.getClientArea
+        w, h = client_area.width, client_area.height
+        @app.dsl.top_slot.width, @app.dsl.top_slot.height = w, h
         @app.real.setSize w, h
+        @app.real.layout
       end
 
       def controlMoved(e)

lib/shoes/swt/layout.rb
@@ -3,11 +3,14 @@ module Shoes::Swt
     attr_accessor :top_slot
 
     def layout *args
+      # Note: This is a Shoes::App
       app = @top_slot.app
       w, h = app.width, app.height
       scrollable_height = contents_alignment @top_slot
+      # Note: This is a Shoes::Swt::App
       app = app.gui
-      app.real.setSize w, [scrollable_height, h].max
+      size = app.real.compute_trim 0, 0, w, [scrollable_height, h].max
+      app.real.set_size(size.width, size.height)
       vb = app.shell.getVerticalBar
       vb.setVisible(scrollable_height > h)
       if scrollable_height > h

spec/shoes/app_spec.rb
@@ -30,10 +30,10 @@ describe Shoes::App do
 
     it "should set accessors from opts", :qt do
       input_blk = Proc.new {}
-      args = {:width => 1, :height => 2, :title => "Shoes::App Spec", :resizable => false}
+      args = {:width => 90, :height => 2, :title => "Shoes::App Spec", :resizable => false}
       Shoes::App.any_instance.stub(:flow)
       app = Shoes::App.new args, &input_blk
-      app.width.should == 1
+      app.width.should == 90
       app.height.should == 2
       app.app_title.should == "Shoes::App Spec"
       app.resizable.should be_false